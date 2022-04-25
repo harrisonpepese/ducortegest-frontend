bf79c18c3bfb1fb284c78a8ed3e5c5435a676cb0
Pepese <harrisonpepese@gmail.com>
2022-04-24 23:51:43 -0300

    correção url

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const http = axios.create({
-  baseURL: process.env.BACKEND_URL || "http://localhost:3000/",
+  baseURL: "https://tcc-barbeiro-backend.herokuapp.com/",
 });
 export default http;
